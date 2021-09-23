f8ec83b52ff70907a1affcd6d08835f11c92b3c4
Laurin Fischer <aur@zurich.ibm.com>
2021-09-23 10:00:15 +0200

    create initial repo structure

---
+++ setup.py
@@ -0,0 +1,61 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import os
+import sys
+import setuptools
+import inspect
+
+long_description = """Qiskit cold atom is an open-source library to describe cold atomic quantum 
+experiments in a gate- and circuit-based framework."""
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+VERSION_PATH = os.path.join(os.path.dirname(__file__), "qiskit_cold_atom", "VERSION.txt")
+with open(VERSION_PATH, "r") as version_file:
+    VERSION = version_file.read().strip()
+
+setuptools.setup(
+    name="qiskit-cold-atom",
+    version=VERSION,
+    description="Integration of cold atomic experiments into the Qiskit SDK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Qiskit-Partners/qiskit-cold-atom",
+    author="Qiskit cold atom development team",
+    author_email="hello@qiskit.org",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit sdk quantum cold atoms",
+    packages=setuptools.find_packages(
+        include=["qiskit_cold_atom", "qiskit_cold_atom.*"]
+    ),
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    python_requires=">=3.6",
+    zip_safe=False,
+)
