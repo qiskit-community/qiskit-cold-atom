91d3781eae9f06717a925b1266711c8f60b65f20
Daniel Egger <deg@zurich.ibm.com>
2021-09-27 21:44:52 +0200

    * Simple transpiler pass to simplify single-spin gates.

---
+++ qiskit_cold_atom/transpiler/single_spin_gates.py
@@ -0,0 +1,89 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""A transpiler pass to simplify single-qubit gates."""
+
+from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.transpiler.exceptions import TranspilerError
+
+from qiskit_cold_atom.spins.spins_gate_library import LXGate, LYGate, LZGate, LZ2Gate
+
+
+class Optimize1SpinGates(TransformationPass):
+    """Simplify single-spin spin gates.
+
+    The simplification of these gates is done by adding the angle of identical and
+    consecutive gates together. For example, the circuit
+
+    .. parsed-literal::
+
+                ┌──────────┐┌──────────┐┌──────────┐┌──────────┐┌──────────┐┌──────────┐
+        spin_0: ┤ Rlx(π/2) ├┤ Rlx(π/2) ├┤ Rlx(π/2) ├┤ Rly(π/2) ├┤ Rly(π/2) ├┤ Rlx(π/2) ├
+                └──────────┘└──────────┘└──────────┘└──────────┘└──────────┘└──────────┘
+
+    will be transpiled to
+
+    .. parsed-literal::
+
+                ┌───────────┐┌────────┐┌──────────┐
+        spin_0: ┤ Rlx(3π/2) ├┤ Rly(π) ├┤ Rlx(π/2) ├
+                └───────────┘└────────┘└──────────┘
+
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._gate_names = ["rLx", "rLy", "rLz", "rLz2"]
+
+    def run(self, dag):
+        """Run the Optimize1qSpinGates pass on `dag`.
+
+        Args:
+            dag (DAGCircuit): the DAG to be optimized.
+
+        Returns:
+            DAGCircuit: the optimized DAG.
+        """
+
+        for gate_name in self._gate_names:
+            runs = dag.collect_runs(gate_name)
+
+            for run in runs:
+                if len(run) == 1:
+                    continue
+
+                # Sum the angles of the same single-qubit gates.
+                total_angle = 0.0
+                for dag_node in run:
+                    total_angle += dag_node.op.params[0]
+
+                if gate_name == "rLx":
+                    new_op = LXGate(total_angle)
+                elif gate_name == "rLy":
+                    new_op = LYGate(total_angle)
+                elif gate_name == "rLz":
+                    new_op = LZGate(total_angle)
+                elif gate_name == "rLz2":
+                    new_op = LZ2Gate(total_angle)
+                else:
+                    raise TranspilerError(
+                        f"Could not use the basis {self._gate_names}."
+                    )
+
+                dag.substitute_node(run[0], new_op, inplace=True)
+
+                # Delete the remaining nodes
+                for current_node in run[1:]:
+                    dag.remove_op_node(current_node)
+
+        return dag
---
+++ test/transpiler/test_transpiler.py
@@ -0,0 +1,52 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Testing module for transpiling."""
+
+import numpy as np
+
+from qiskit import QuantumCircuit
+from qiskit.transpiler import PassManager
+from qiskit.test import QiskitTestCase
+
+from qiskit_cold_atom.transpiler import Optimize1SpinGates
+
+
+class TestSpinTranspilation(QiskitTestCase):
+    """Test class for spin-based transpilation."""
+
+    def test_optimize_1s_gates(self):
+        """Test the single-spin gate transpilation."""
+
+        circ = QuantumCircuit(1)
+        circ.lx(np.pi / 2, 0)
+        circ.lx(np.pi / 2, 0)
+        circ.lx(np.pi / 2, 0)
+        circ.ly(np.pi / 2, 0)
+        circ.ly(np.pi / 2, 0)
+        circ.lz2(np.pi / 4, 0)
+        circ.lz2(np.pi / 4, 0)
+        circ.lz2(np.pi / 4, 0)
+        circ.lx(np.pi / 2, 0)
+
+        pass_manager = PassManager(Optimize1SpinGates())
+
+        circ_new = pass_manager.run(circ)
+
+        self.assertEqual(circ_new.count_ops()["rLx"], 2)
+        self.assertEqual(circ_new.count_ops()["rLy"], 1)
+        self.assertEqual(circ_new.count_ops()["rLz2"], 1)
+
+        self.assertTrue(np.allclose(circ_new.data[0][0].params[0], 3 * np.pi / 2))
+        self.assertTrue(np.allclose(circ_new.data[1][0].params[0], np.pi))
+        self.assertTrue(np.allclose(circ_new.data[2][0].params[0], 3 * np.pi / 4))
+        self.assertTrue(np.allclose(circ_new.data[3][0].params[0], np.pi / 2))
