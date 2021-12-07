6c605f8953fc656dca4fe448cc005a600451a7d3
Laurin Fischer <59973767+LaurinFischer@users.noreply.github.com>
2021-12-07 20:36:14 +0100

    Update qiskit_cold_atom/providers/collective_spin_backend.py
    Co-authored-by: Daniel J. Egger <38065505+eggerdj@users.noreply.github.com>

--- qiskit_cold_atom/providers/collective_spin_backend.py
+++ qiskit_cold_atom/providers/collective_spin_backend.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Mock backend to simulate collective spin experiments."""
+"""Backend to emulate collective spin experiments."""
 
 from typing import Optional
 
